6dc73993a428afac60b40a1f68236e3f34905444
Ivan Kurinnyi <vanyakurinnoy13@gmail.com>
2026-06-21 10:06:53 +0200

    Update main_menu.py
    bg update

--- main_menu.py
+++ main_menu.py
@@ -1,1 +1,14 @@
 # Кнопка виходу Кнопка грати Кнопкаи налаштувань Фон
+from customtkinter import *
+from PIL import Image
+class MainMenu(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Main Menu")
+        self.geometry("800x600")
+        self.configure(bg="black")
+
+        self.bg_image = Image.open("background.jpg")
+        self.bg_photo = CTkImage(self.bg_image, size=(800, 600))
+        self.bg_label = CTkLabel(self, image=self.bg_photo)
+        self.bg_label.place(x=0, y=0)
