af3196980d777d17635c99ff5f0eb42461a9b432
Ivan Kurinnyi <vanyakurinnoy13@gmail.com>
2026-06-25 13:33:29 +0200

    background

--- play_menu.py
+++ play_menu.py
@@ -10,7 +10,7 @@ class PlayMenu(CTk):
         self.resizable(False, False)
 
         # Background
-        self.bg_image = Image.open(r"assets\img\backgrounds\nickname_menu_bg.jpg")
+        self.bg_image = Image.open(r"assets\img\backgrounds\play_menu_bg.jpg")
         self.bg_photo = CTkImage(self.bg_image, size=(1000, 600))
         self.bg_label = CTkLabel(self, image=self.bg_photo, text="")
         self.bg_label.place(x=0, y=0)
